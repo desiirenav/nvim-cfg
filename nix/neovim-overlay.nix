57d1c855f2056fa2e8135fde5fe464ff56ea2579
desiirenav <desiirenav@hotmail.com>
2026-07-16 01:23:28 -0400

    Remove luasnip from Neovim plugin list
    Removed luasnip plugin from the Neovim overlay.

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -32,7 +32,6 @@ with final.pkgs.lib; let
     # plugins from nixpkgs go in here.
     # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
     nvim-treesitter.withAllGrammars
-    luasnip # snippets | https://github.com/l3mon4d3/luasnip/
     # nvim-cmp (autocompletion) and extensions
     nvim-cmp # https://github.com/hrsh7th/nvim-cmp
     cmp_luasnip # snippets autocompletion extension for nvim-cmp | https://github.com/saadparwaiz1/cmp_luasnip/
