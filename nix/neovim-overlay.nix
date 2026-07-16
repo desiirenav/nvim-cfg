d0d448dadb0205156deb7ba6ff472b9209447d72
desiirenav <desiirenav@hotmail.com>
2026-07-16 01:29:46 -0400

    Remove vim-unimpaired plugin
    Removed vim-unimpaired plugin from Neovim overlay.

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -52,7 +52,6 @@ with final.pkgs.lib; let
     # language support
     # ^ language support
     # navigation/editing enhancement plugins
-    vim-unimpaired # predefined ] and [ navigation keymaps | https://github.com/tpope/vim-unimpaired/
     eyeliner-nvim # Highlights unique characters for f/F and t/T motions | https://github.com/jinh0/eyeliner.nvim
     nvim-surround # https://github.com/kylechui/nvim-surround/
     nvim-treesitter-textobjects # https://github.com/nvim-treesitter/nvim-treesitter-textobjects/
